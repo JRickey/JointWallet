cfd35b5859d7ba93dd1e3f19b19ef0f234bdbe3f
Jack Rickey <jackrickey@Jacks-MacBook-Pro.local>
2022-12-07 23:17:34 -0500

    removed owner1

--- src/JointWallet.sol
+++ src/JointWallet.sol
@@ -3,24 +3,22 @@ pragma solidity ^0.8.13;
 import "solmate/auth/Owned.sol";
 
 contract JointWallet is Owned {
-    address payable public owner1;
     address payable public owner2;
 
     constructor(address coOwner) Owned(msg.sender) {
-        owner1 = payable(msg.sender);
         owner2 = payable(coOwner);
     }
 
     receive() external payable {
-        require(msg.sender == owner1 || msg.sender == owner2, "Caller is not authorized to deposit");
+        require(msg.sender == owner || msg.sender == owner2, "Caller is not authorized to deposit");
     }
 
     fallback() external payable {
-        require(msg.sender == owner1 || msg.sender == owner2, "Caller is not authorized to deposit");
+        require(msg.sender == owner || msg.sender == owner2, "Caller is not authorized to deposit");
     }
 
     function withdraw(uint _amount) external {
-        require(msg.sender == owner1 || msg.sender == owner2, "caller is not authorized to withdraw");
+        require(msg.sender == owner || msg.sender == owner2, "caller is not authorized to withdraw");
         (bool sent, bytes memory data) = msg.sender.call{value: _amount}("");
         require(sent == true);
     }
